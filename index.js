198eb3a1153ca576c0ee9abce735f76908ea11cc
mjapanwala <55627647+mjapanwala@users.noreply.github.com>
2019-12-08 18:22:24 -0800

    phase 1

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict'
-let express = require('express');
+let express = require ('express')
 let app = express();
 let port = 3000;
 function handleServerListen() {
     console.log(`Server is listening on port ${port}`);
   }
   app.listen(port, handleServerListen);
+  
   
