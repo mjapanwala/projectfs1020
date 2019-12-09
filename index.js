b94c11201f9370c745a714a2ad9a5e518eb9add6
mjapanwala <55627647+mjapanwala@users.noreply.github.com>
2019-12-08 17:22:22 -0800

    commit

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+let express = require('express');
+let app = express();
+let port = 3000;
+function handleServerListen() {
+    console.log(`Server is listening on port ${port}`);
+  }
+  app.listen(port, handleServerListen);
+  
