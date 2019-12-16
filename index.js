b60187a7e6b9eddcf55b049549e8bc8894129d67
mjapanwala <55627647+mjapanwala@users.noreply.github.com>
2019-12-15 16:40:26 -0800

    update file

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-'use strict'
-let express = require ('express')
+
+let express = require('express')
 let app = express();
-let port = 3000;
-function handleServerListen() {
-    console.log(`Server is listening on port ${port}`);
-  }
-  app.listen(port, handleServerListen);
-  
-  
+let port = 4001;
+
+app.use(express.json());
+
+app.listen(port, handleServerListen => {
+  console.log(`Port is listening on ${port}`)
+});
+
+// Get all the data
+app.get('/', (req, res) => {
+  res.send('get all the data')
+});
+
+//Create submission
+app.post('/user/submissions', (req, res) => {
+  res.send('Create submission')
+})
+//Create new user
+app.post('/userform', (req, res) => {
+  console.log(req.body); // { a: 1 }
+  res.send('new user')
+})
+
+//Route to log a registered user in to create a session.
+app.post('/user/session', (req, res) => {
+  res.send('new session')
+})
+// Route to get a listing of all submissions.
+app.get('/submissions', (req, res) => {
+    res.send('All submissions')
+});
+
+// Default error handling
+app.use(function (error, req, res, next) {
+  console.error(error); 
+  res.sendStatus(500); 
+});
+
